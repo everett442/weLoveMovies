47828fe7fbec78e1c5bb42bf7a2db9c86355c410
everett442 <everett442@hotmail.com>
2023-07-24 02:05:27 -0400

    Modified movies route

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -5,12 +5,12 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 router.route("/").get(controller.list).all(methodNotAllowed);
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 router
-  .route("/:movieId/theaters")
-  .get(controller.readTheatersByMovie)
-  .all(methodNotAllowed);
-router
   .route("/:movieId/reviews")
   .get(controller.readReviewsByMovie)
+  .all(methodNotAllowed);
+router
+  .route("/:movieId/theaters")
+  .get(controller.readTheatersByMovie)
   .all(methodNotAllowed);
 
 module.exports = router;
