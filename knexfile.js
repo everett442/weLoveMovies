673c5c1a67a145329ac85312f9710d27a1d5323e
everett442 <everett442@hotmail.com>
2023-07-24 00:27:30 -0400

    Modified knex to work with env

--- knexfile.js
+++ knexfile.js
@@ -2,32 +2,24 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: URL,
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: "./src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: "./src/db/seeds",
     },
   },
 
   production: {
     client: "postgresql",
-    connection: URL,
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -43,10 +35,10 @@ module.exports = {
       filename: ":memory:",
     },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: "./src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: "./src/db/seeds",
     },
     useNullAsDefault: true,
   },
